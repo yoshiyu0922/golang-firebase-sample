b86becad1d0a7aa8460850a266867d661f23d0d7
yoshiyu0922 <yoshi.yoshiyu0922@gmail.com>
2020-10-31 10:25:40 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module example.com/firebase-sample
+
+go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.3.0 // indirect
+	cloud.google.com/go/storage v1.12.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/joho/godotenv v1.3.0
+	google.golang.org/api v0.34.0
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	firebase "firebase.google.com/go"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"log"
+	"os"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	// .envの読み取り
+	err := godotenv.Load(".env")
+	if err != nil {
+		// .env読めなかった場合の処理
+	}
+
+	apiKey := os.Getenv("APIKEY")
+	fmt.Println(apiKey)
+	authDomain := os.Getenv("AUTHDOMAIN")
+	databaseUrl := os.Getenv("DATABASEURL")
+	projectId := os.Getenv("PROJECTID")
+	storageBucket := os.Getenv("STORAGEBUCKET")
+	messagingSenderId := os.Getenv("MESSAGINGSENDERID")
+	appId := os.Getenv("APPID")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{
+			"apiKey":            apiKey,
+			"authDomain":        authDomain,
+			"databaseUrl":       databaseUrl,
+			"projectId":         projectId,
+			"storageBucket":     storageBucket,
+			"messagingSenderId": messagingSenderId,
+			"appId":             appId,
+		})
+	})
+
+	router.POST("/auth", authByFirebase)
+
+	router.Run()
+}
+
+func authByFirebase(ctx *gin.Context) {
+	opt := option.WithCredentialsFile("secretkey.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Panic(fmt.Errorf("error initializing app: %v", err))
+	}
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	// トークンを確認
+	idToken := ctx.PostForm("id_token")
+	token, err := client.VerifyIDToken(context.Background(), idToken)
+	if err != nil {
+		log.Fatalf("error verifying ID token: %v\n", err)
+	}
+
+	email := token.Claims["email"]
+	log.Printf("email: %v\n", email)
+
+	ctx.HTML(200, "result.html", gin.H{
+		"idToken": idToken,
+		"email":   email,
+	})
+}
